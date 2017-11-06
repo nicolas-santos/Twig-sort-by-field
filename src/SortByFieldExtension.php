3eb345925df9c8b8d7685ce2dfc1bafe27d13b4b
nico <ns@memoclic.net>
2017-11-06 17:24:41 +0100

    Disable SortByFieldExtension during tests

--- src/SortByFieldExtension.php
+++ src/SortByFieldExtension.php
@@ -13,16 +13,28 @@ use Exception;
  *
  * I have extended it to also sort array structures
  */
-class SortByFieldExtension extends \Twig_Extension {
+class SortByFieldExtension extends \Twig_Extension
+{
 
-    public function getName() {
+
+    /** @var String */
+    private $environment;
+
+    public function __construct($environment)
+    {
+        $this->environment = $environment;
+    }
+
+    public function getName()
+    {
         return 'sortbyfield';
     }
 
-    public function getFilters() {
-        return array(
-            new \Twig_SimpleFilter('sortbyfield', array($this, 'sortByFieldFilter'))
-        );
+    public function getFilters()
+    {
+        return [
+            new \Twig_SimpleFilter('sortbyfield', [$this, 'sortByFieldFilter']),
+        ];
     }
 
     /**
@@ -30,65 +42,84 @@ class SortByFieldExtension extends \Twig_Extension {
      *
      * Usage: {% for entry in master.entries|sortbyfield('ordering', 'desc') %}
      */
-    public function sortByFieldFilter($content, $sort_by = null, $direction = 'asc') {
+    public function sortByFieldFilter($content, $sort_by = null, $direction = 'asc')
+    {
 
-        if (is_a($content, 'Doctrine\Common\Collections\Collection')) {
+        if ( is_a($content, 'Doctrine\Common\Collections\Collection') ) {
             $content = $content->toArray();
         }
 
-        if (!is_array($content)) {
+        if ( ! is_array($content) ) {
             throw new \InvalidArgumentException('Variable passed to the sortByField filter is not an array');
-        } elseif (count($content) < 1) {
+        } elseif ( count($content) < 1 ) {
             return $content;
-        } elseif ($sort_by === null) {
+        } elseif ( $sort_by === null ) {
             throw new Exception('No sort by parameter passed to the sortByField filter');
-        } elseif (!self::isSortable(current($content), $sort_by)) {
+        } elseif ( ! self::isSortable(current($content), $sort_by) ) {
+            if($this->environment === 'test'){
+                return $content;
+            }
             throw new Exception('Entries passed to the sortByField filter do not have the field "' . $sort_by . '"');
         } else {
             // Unfortunately have to suppress warnings here due to __get function
             // causing usort to think that the array has been modified:
             // usort(): Array was modified by the user comparison function
-            @usort($content, function ($a, $b) use ($sort_by, $direction) {
-                $flip = ($direction === 'desc') ? -1 : 1;
+            @usort(
+                $content,
+                function ($a, $b) use ($sort_by, $direction) {
+                    $flip = ($direction === 'desc') ? -1 : 1;
 
-                if (is_array($a))
-                    $a_sort_value = $a[$sort_by];
-                else if (method_exists($a, 'get' . ucfirst($sort_by)))
-                    $a_sort_value = $a->{'get' . ucfirst($sort_by)}();
-                else
-                    $a_sort_value = $a->$sort_by;
+                    if ( is_array($a) ) {
+                        $a_sort_value = $a[$sort_by];
+                    } else {
+                        if ( method_exists($a, 'get' . ucfirst($sort_by)) ) {
+                            $a_sort_value = $a->{'get' . ucfirst($sort_by)}();
+                        } else {
+                            $a_sort_value = $a->$sort_by;
+                        }
+                    }
 
-                if (is_array($b))
-                    $b_sort_value = $b[$sort_by];
-                else if (method_exists($b, 'get' . ucfirst($sort_by)))
-                    $b_sort_value = $b->{'get' . ucfirst($sort_by)}();
-                else
-                    $b_sort_value = $b->$sort_by;
+                    if ( is_array($b) ) {
+                        $b_sort_value = $b[$sort_by];
+                    } else {
+                        if ( method_exists($b, 'get' . ucfirst($sort_by)) ) {
+                            $b_sort_value = $b->{'get' . ucfirst($sort_by)}();
+                        } else {
+                            $b_sort_value = $b->$sort_by;
+                        }
+                    }
 
-                if ($a_sort_value == $b_sort_value) {
-                    return 0;
-                } else if ($a_sort_value > $b_sort_value) {
-                    return (1 * $flip);
-                } else {
-                    return (-1 * $flip);
+                    if ( $a_sort_value == $b_sort_value ) {
+                        return 0;
+                    } else {
+                        if ( $a_sort_value > $b_sort_value ) {
+                            return (1 * $flip);
+                        } else {
+                            return (-1 * $flip);
+                        }
+                    }
                 }
-            });
+            );
         }
         return $content;
     }
 
     /**
      * Validate the passed $item to check if it can be sorted
-     * @param $item mixed Collection item to be sorted
+     *
+     * @param $item  mixed Collection item to be sorted
      * @param $field string
+     *
      * @return bool If collection item can be sorted
      */
-    private static function isSortable($item, $field) {
-        if (is_array($item))
+    private static function isSortable($item, $field)
+    {
+        if ( is_array($item) ) {
             return array_key_exists($field, $item);
-        elseif (is_object($item))
+        } elseif ( is_object($item) ) {
             return isset($item->$field) || property_exists($item, $field);
-        else
+        } else {
             return false;
+        }
     }
 }
